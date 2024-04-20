45a0e49b738e6df58fb33fc827a61ec0ecf4e668
Sadia Tasnim <tasnimsadia259@gmail.com>
2024-04-20 16:35:17 +0600

    card view added, background colour added

--- NiagaraView/ContentView.swift
+++ NiagaraView/ContentView.swift
@@ -9,37 +9,55 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack(alignment: .leading, spacing:20) {
-            Image("NiagaraFalls")
-                .resizable()
-                .frame(height: 250)
-                .aspectRatio(contentMode: .fit)
-                .cornerRadius(15)
-                .padding(.horizontal,2)
-                
-            HStack{
-                Text("Niagara Falls")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .padding()
-                
-                Spacer()
-                
-                HStack(spacing: 2) {
-                        Image(systemName: "star.fill")
-                        Image(systemName: "star.fill")
-                        Image(systemName: "star.fill")
-                        Image(systemName: "star.fill")
-                        Image(systemName: "star.leadinghalf.filled")
-                        .padding(.trailing)
-                    }
-            }
+        ZStack {
+            LinearGradient(colors: [Color.white,Color("Color2")], startPoint: .topLeading, endPoint: .bottomTrailing)
+                .ignoresSafeArea()
             
-                Text("Come visit the falls for an experience of a lifetime.")
-                    .font(.system(size: 20, weight: .medium, design: .default))
-                    .padding()
+            VStack(alignment: .leading, spacing:15) {
+                Image("NiagaraFalls")
+                    .resizable()
+                    .frame(height: 250)
+                    .aspectRatio(contentMode: .fit)
+                    .cornerRadius(15)
+                    .padding(10)
+                    
+                HStack{
+                    Text("Niagara Falls")
+                        .font(.title)
+                        .fontWeight(.bold)
+                        .padding()
+                    
+                    Spacer()
+                    
+                    HStack(spacing: 2) {
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.leadinghalf.filled")
+                            .padding(.trailing)
+                        }
+                    .foregroundColor(.yellow)
+                }
+                
+                    Text("Come visit the falls for an experience of a lifetime.")
+                        .font(.system(size: 20, weight: .medium, design: .default))
+                        .padding()
+                
+                HStack {
+                    Spacer()
+                    Image(systemName: "binoculars.fill")
+                    Image(systemName: "fork.knife")
+                        .padding(.trailing)
+                }.foregroundColor(.gray)
+            }
+            .background(Rectangle()
+                .foregroundColor(.white)
+                .cornerRadius(10)
+                .shadow(radius: 12)
+            
+            )
         }
-        
     }
 }
 
