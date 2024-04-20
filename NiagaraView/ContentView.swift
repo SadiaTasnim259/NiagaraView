ca28e2b2527b17b37a7d9bd3cbbb9c6e42c072eb
Sadia Tasnim <tasnimsadia259@gmail.com>
2024-04-20 23:16:04 +0600

    UI updated

--- NiagaraView/ContentView.swift
+++ NiagaraView/ContentView.swift
@@ -10,18 +10,19 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         ZStack {
-            LinearGradient(colors: [Color.white,Color("Color2")], startPoint: .topLeading, endPoint: .bottomTrailing)
+            LinearGradient(colors: [Color.white, Color("Color2")], startPoint: .topLeading, endPoint: .bottomTrailing)
                 .ignoresSafeArea()
             
-            VStack(alignment: .leading, spacing:15) {
+            VStack( spacing: 15) {
                 Image("NiagaraFalls")
                     .resizable()
                     .frame(height: 250)
                     .aspectRatio(contentMode: .fit)
                     .cornerRadius(15)
                     .padding(10)
-                    
-                HStack{
+                
+                
+                HStack {
                     Text("Niagara Falls")
                         .font(.title)
                         .fontWeight(.bold)
@@ -30,33 +31,38 @@ struct ContentView: View {
                     Spacer()
                     
                     HStack(spacing: 2) {
-                            Image(systemName: "star.fill")
-                            Image(systemName: "star.fill")
-                            Image(systemName: "star.fill")
-                            Image(systemName: "star.fill")
-                            Image(systemName: "star.leadinghalf.filled")
+                        Image(systemName: "star.fill")
+                        Image(systemName: "star.fill")
+                        Image(systemName: "star.fill")
+                        Image(systemName: "star.fill")
+                        Image(systemName: "star.leadinghalf.filled")
                             .padding(.trailing)
-                        }
+                    }
                     .foregroundColor(.yellow)
                 }
                 
-                    Text("Come visit the falls for an experience of a lifetime.")
-                        .font(.system(size: 20, weight: .medium, design: .default))
-                        .padding()
+                Text("Come visit the falls for an experience of a lifetime.")
+                    .font(.system(size: 20, weight: .medium, design: .default))
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
                 
                 HStack {
                     Spacer()
                     Image(systemName: "binoculars.fill")
                     Image(systemName: "fork.knife")
                         .padding(.trailing)
-                }.foregroundColor(.gray)
+                }
+                .foregroundColor(.gray)
+                .padding(.bottom)
+                
             }
-            .background(Rectangle()
-                .foregroundColor(.white)
-                .cornerRadius(10)
-                .shadow(radius: 12)
-            
-            )
+            .background {
+                Rectangle()
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                    .shadow(radius: 15)
+                    .padding(.horizontal, 4)
+            }
         }
     }
 }
