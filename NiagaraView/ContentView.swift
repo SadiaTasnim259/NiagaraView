605a084b394b31e6be08ab073d25020ce3c8882b
Sadia Tasnim <tasnimsadia259@gmail.com>
2024-04-20 09:46:51 +0600

    Initial Commit

---
+++ NiagaraView/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  NiagaraView
+//
+//  Created by Sadia on 20/4/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NiagaraView/NiagaraViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  NiagaraViewApp.swift
+//  NiagaraView
+//
+//  Created by Sadia on 20/4/24.
+//
+
+import SwiftUI
+
+@main
+struct NiagaraViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
